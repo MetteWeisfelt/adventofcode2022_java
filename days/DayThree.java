d0bd1bf531a9e13c52c35c72156aa3a2fd19bd90
MetteWeisfelt <metteweisfelt@gmail.com>
2022-12-06 23:13:45 +0100

    completed day 3 challenge 2

--- days/DayThree.java
+++ days/DayThree.java
@@ -51,16 +51,46 @@ public class DayThree {
         File file;
         BufferedReader br;
         String line;
+        int count = 0;
+        HashSet<Character> setList = new HashSet<Character>();
+        HashSet<Character> dupeList = new HashSet<Character>();
+        int priorities = 0;
 
         try {
             file = new File("C:\\Users\\mcwei\\Documents\\Personal\\Projects\\adventofcode2022\\inputs\\inputdaythree.txt");
             br = new BufferedReader(new FileReader(file));
 
             while((line = br.readLine()) != null){
-                
+                if(count == 0) {
+                    for (int i = 0; i < line.length(); i++) {
+                        setList.add(line.charAt(i));
+                    }
+                    count++;
+                } else if (count == 1){
+                    for (int i = 0; i < line.length(); i++) {
+                        if (setList.contains(line.charAt(i))) {
+                            dupeList.add(line.charAt(i));
+                        }
+                    }
+                    count++;
+                } else {
+                    for (int i = 0; i < line.length(); i++){
+                        if(dupeList.contains(line.charAt(i))){
+                            if(Character.isUpperCase(line.charAt(i))){
+                                priorities += (int) line.charAt(i) - 38;
+                            } else {
+                                priorities += (int) line.charAt(i) - 96;
+                            }
+                            dupeList.clear();
+                            count = 0;
+                            setList.clear();
+                            break;
+                        }
+                    }
+                }
             }
 
-            System.out.println(">>> answer day three challenge 2: " );
+            System.out.println(">>> answer day three challenge 2: " + priorities);
 
         } catch (IOException e) {
             throw new RuntimeException(e);
