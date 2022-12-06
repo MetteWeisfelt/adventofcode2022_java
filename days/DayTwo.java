bbd6e5dcb9487cc11d11d5af40718f90d2870073
MetteWeisfelt <metteweisfelt@gmail.com>
2022-12-06 20:16:27 +0100

    day two completed

--- days/DayTwo.java
+++ days/DayTwo.java
@@ -13,8 +13,6 @@ public class DayTwo {
         String line;
         String[] selection;
         int totalScore = 0;
-        int playerScore = 0;
-        int elfScore = 0;
         HashMap<String, Integer> scores = new HashMap<String, Integer>();
 
         scores.put("X", 1);
@@ -22,7 +20,7 @@ public class DayTwo {
         scores.put("Z", 3);
 
         try {
-            file = new File("C:\\Projects\\adventofcode\\adventofcode2022\\adventofcode2022_java\\inputs\\inputdaytwo.txt");
+            file = new File("C:\\Users\\mcwei\\Documents\\Personal\\inputdaytwo.txt");
             br = new BufferedReader(new FileReader(file));
 
             while((line = br.readLine()) != null){
@@ -40,6 +38,28 @@ public class DayTwo {
     public static void runChallenge2(){
         System.out.println(">>> day 2 challenge 2 is starting now...");
 
+        File file;
+        BufferedReader br;
+        String line;
+        String[] selection;
+        int totalScore = 0;
+
+        try {
+            file = new File("C:\\Users\\mcwei\\Documents\\Personal\\inputdaytwo.txt");
+            br = new BufferedReader(new FileReader(file));
+
+            while((line = br.readLine()) != null){
+                selection = line.split("\\s+");
+
+                totalScore += getPointsChallenge2(selection);
+            }
+
+            System.out.println(">>> answer day two challenge 2: " + totalScore);
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
     }
 
     static int getRoundPoints(String[] selection){
@@ -54,5 +74,40 @@ public class DayTwo {
         } else {
             return 0;
         }
+    }
+
+    static int getPointsChallenge2(String[] selection){
+        int points = 0;
+
+        switch (selection[1]) {
+            case "Z" -> {
+                points += 6;
+                switch (selection[0]) {
+                    case "A" -> points += 2;
+                    case "B" -> points += 3;
+                    case "C" -> points += 1;
+                }
+                return points;
+            }
+            case "Y" -> {
+                points += 3;
+                switch (selection[0]) {
+                    case "A" -> points += 1;
+                    case "B" -> points += 2;
+                    case "C" -> points += 3;
+                }
+                return points;
+            }
+            case "X" -> {
+                points += 0;
+                switch (selection[0]) {
+                    case "A" -> points += 3;
+                    case "B" -> points += 1;
+                    case "C" -> points += 2;
+                }
+                return points;
+            }
+        }
+        return 0;
     }
 }
