adca31d59372869eae8bf9f79ce4ae8ae8660fa4
MetteWeisfelt <metteweisfelt@gmail.com>
2022-12-07 22:08:27 +0100

    day 4 completed

--- Start.java
+++ Start.java
@@ -3,7 +3,7 @@ import days.*;
 public class Start {
 
     public static void main(String[] args){
-        DaySeven.runChallenge1();
-        DaySeven.runChallenge2();
+        DayFour.runChallenge1();
+        DayFour.runChallenge2();
     }
 }
--- days/DayFour.java
+++ days/DayFour.java
@@ -11,13 +11,30 @@ public class DayFour {
 
         File file;
         BufferedReader br;
-
+        int count = 0;
+        String line;
 
         try {
             file = new File("C:\\Users\\mcwei\\Documents\\Personal\\Projects\\adventofcode2022\\inputs\\inputdayfour.txt");
             br = new BufferedReader(new FileReader(file));
 
-            System.out.println(">>> answer day four challenge 1: ");
+            while((line = br.readLine()) != null) {
+                String[] ranges = line.split(",");
+                String[] elementOne = ranges[0].split("-");
+                String[] elementTwo = ranges[1].split("-");
+
+                int oneFirst = Integer.parseInt(elementOne[0]);
+                int twoFirst = Integer.parseInt(elementTwo[0]);
+                int oneSecond = Integer.parseInt(elementOne[1]);
+                int twoSecond = Integer.parseInt(elementTwo[1]);
+
+                if(((oneFirst <= twoFirst) && (oneSecond >= twoSecond))
+                    || ((twoFirst <= oneFirst) && (twoSecond >= oneSecond))) {
+                    count++;
+                }
+            }
+
+            System.out.println(">>> answer day 4 challenge 1: " + count);
 
         } catch (IOException e) {
             throw new RuntimeException(e);
@@ -29,12 +46,33 @@ public class DayFour {
 
         File file;
         BufferedReader br;
+        String line;
+        int count = 0;
 
         try {
             file = new File("C:\\Users\\mcwei\\Documents\\Personal\\Projects\\adventofcode2022\\inputs\\inputdayfour.txt");
             br = new BufferedReader(new FileReader(file));
 
-            System.out.println(">>> answer day four challenge 2: ");
+            while((line = br.readLine()) != null) {
+                String[] ranges = line.split(",");
+                String[] elementOne = ranges[0].split("-");
+                String[] elementTwo = ranges[1].split("-");
+
+                int oneFirst = Integer.parseInt(elementOne[0]);
+                int twoFirst = Integer.parseInt(elementTwo[0]);
+                int oneSecond = Integer.parseInt(elementOne[1]);
+                int twoSecond = Integer.parseInt(elementTwo[1]);
+
+                if (((twoFirst >= oneFirst) && (twoFirst <= oneSecond))
+                    || ((twoSecond >= oneFirst) && (twoSecond <= oneSecond))){
+                    count++;
+                } else if (((oneFirst >= twoFirst) && (oneFirst <= twoSecond))
+                    || ((oneSecond >= twoFirst) && (oneSecond <= twoSecond))){
+                    count++;
+                }
+            }
+
+            System.out.println(">>> answer day 4 challenge 2: " + count);
 
         } catch (IOException e) {
             throw new RuntimeException(e);
